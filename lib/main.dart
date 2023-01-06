952d57c443789898462de09059d11d721f385837
Tushar Mishra <you@example.com>
2023-01-06 20:08:11 +0530

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:xpense/pages/home.dart';
+import 'package:xpense/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Xpense',
+      theme: ThemeData(
+          fontFamily: 'HindSiliguri',
+          brightness: Brightness.light,
+          primaryColor: AppColors.primary),
+      home: const Home(),
+    );
+  }
+}
