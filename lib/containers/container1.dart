6f816e7192084aabd1d356d557b12b0bf1c9a781
Tushar Mishra <you@example.com>
2023-01-07 00:06:57 +0530

    initial

---
+++ lib/containers/container1.dart
@@ -0,0 +1,165 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:xpense/utils/colors.dart';
+import 'package:xpense/utils/constants.dart';
+
+class Container1 extends StatefulWidget {
+  const Container1({Key? key}) : super(key: key);
+
+  @override
+  _Container1State createState() => _Container1State();
+}
+
+class _Container1State extends State<Container1> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: MobileContainer1(),
+      desktop: DesktopContainer1(),
+    );
+  }
+
+  //================ MOBILE ===============
+
+  // ignore: non_constant_identifier_names
+  Widget MobileContainer1() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: w! / 10, vertical: 20),
+      child: Column(
+        children: [
+          Container(
+            height: w! / 1.2,
+            width: w! / 1.2,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(illustration1), fit: BoxFit.contain)),
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Track your \nExpenses to \nSave Money',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: w! / 10, fontWeight: FontWeight.bold, height: 1),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Helps you to organize your income and expenses',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 30,
+          ),
+          // ignore: sized_box_for_whitespace
+          Container(
+            height: 45,
+            child: ElevatedButton.icon(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(AppColors.primary)),
+                onPressed: () {},
+                // ignore: prefer_const_constructors
+                icon: Icon(Icons.arrow_drop_down),
+                // ignore: prefer_const_constructors
+                label: Text('Try a Demo')),
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '— Web, iOS and Android',
+            style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+
+  //============== DESKTOP =============
+
+  // ignore: non_constant_identifier_names
+  Widget DesktopContainer1() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: w! / 10, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+              // ignore: avoid_unnecessary_containers
+              child: Container(
+            // color: Colors.red,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Track your \nExpenses to \nSave Money',
+                  style: TextStyle(
+                      fontSize: w! / 20,
+                      fontWeight: FontWeight.bold,
+                      height: 1),
+                ),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Helps you to organize your income and expenses',
+                  style: TextStyle(color: Colors.grey.shade400, fontSize: 16),
+                ),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    // ignore: sized_box_for_whitespace
+                    Container(
+                      height: 45,
+                      child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(AppColors.primary)),
+                          onPressed: () {},
+                          // ignore: prefer_const_constructors
+                          icon: Icon(Icons.arrow_drop_down),
+                          label: const Text('Try a Demo')),
+                    ),
+                    // ignore: prefer_const_constructors
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      '— Web, iOS and Android',
+                      style:
+                          TextStyle(color: Colors.grey.shade400, fontSize: 16),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )),
+          Expanded(
+            child: Container(
+              height: 530,
+              // ignore: prefer_const_constructors
+              decoration: BoxDecoration(
+                  // ignore: prefer_const_constructors
+                  image: DecorationImage(
+                      image: const AssetImage(illustration1),
+                      fit: BoxFit.contain)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+double? h;
+double? w;
+
+const String logo = 'assets/images/logo.png';
+const String illustration1 = 'assets/images/Illustrator-1.png';
+const String illustration2 = 'assets/images/Use anytime when you need.png';
+const String illustration3 = 'assets/images/Image.png';
+const String google = 'assets/images/image 2.png';
+const String fb = 'assets/images/image 1.png';
+const String cocacola = 'assets/images/image 3.png';
+const String samsung = 'assets/images/image 4.png';
+const String dashboard = 'assets/images/Image-1.png';
+const String vector1 = 'assets/images/Vector.png';
+const String vector2 = 'assets/images/Vector-1.png';
